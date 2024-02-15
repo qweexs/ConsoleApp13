ff18f24a4cab883092173ab0c4e5aa4ea1e4ae09
Alenka Chocolate <avkakolina@kait20.ru>
2024-02-15 21:12:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Persona
+{
+    private string name;
+    string fam;
+    string otch;
+    int Vector { get; set; }
+
+    public string Name
+    {
+        get { return string.IsNullOrEmpty(name) ? "..." : name; }
+        set { name = value.Trim().ToUpper(); }
+    }
+    public string Fam
+    {
+        get { return string.IsNullOrEmpty(fam) ? "..." : fam; }
+        set { fam = value; }
+    }
+    public string Otch
+    {
+        get { return string.IsNullOrEmpty(otch) ? "..." : otch; }
+        set { otch = value; }
+    }
+    public Persona()
+    {
+        name = string.Empty;
+        fam = string.Empty;
+        otch = string.Empty;
+    }
+    public Persona(string n) : this() { name = n; }
+    public Persona(string n, string f) : this(n) { fam = f; }
+    public Persona(string f, string n, string ot) : this(n, f) { otch = ot; }
+    public void Show()
+    {
+        Console.WriteLine("{0} {1} {2} ", Fam, Name, Otch);
+
+    }
+    public int _Show()
+    {
+        Console.WriteLine("{0} {1} {2} ", Fam, Name, Otch);
+        return 0;
+    }
+    public void Show(int x)
+    {
+        System.Windows.Forms.MessageBox.Show(
+        string.Format("{0} {1} {2} ", Fam, Name, Otch)
+        );
+    }
+
+    public override string ToString()
+    {
+
+        return string.Format("{0} {1}. {2}. ", Fam, Name.Substring(0, 1), Otch.Substring(0, 1));
+    }
+    public void Init()
+    {
+        Console.Write("Фамилия >>");
+        fam = Console.ReadLine();
+        Console.Write("Имя >>");
+        name = Console.ReadLine();
+        Console.Write("Отчество >>");
+        otch = Console.ReadLine();
+
+    }
+}
---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona();
+            Persona persona2 = new Persona("Иванов");
+            Persona persona3 = new Persona("Петров", "Петр");
+            Persona persona4 = new Persona("Сидоров", "Сидор", "Сидорович");
+
+            persona1.Show();
+            persona2.Show();
+            persona3.Show();
+            persona4.Show();
+
+            Console.ReadKey();
+        }
+}
